cff422f4e2ff7bf51b5efd8434ac963703f7bec6
kuzcode <igormakw228@gmail.com>
2022-08-11 00:59:12 +0300

    BidirectionalMap.Serialization.Json were retargeted to .NET Standard 1.6.
    BidirectionalMap project reference was replaced with NuGet package reference.
    
    System.Text.Json was replaced with Newtonsoft.Json because System.Text.Json is for .NET Standard 2.0+.
    
    BidirectionalMap.Serialization.Json with BidirectionalMap.Serialization.Json.Test were renamed to BidirectionalMap.Json.Newtonsoft and BidirectionalMap.Json.Newtonsoft.Test.
    
    All this has been done because only the old BiMap API requires a special JSON converter.

---
+++ src/BidirectionalMap.Json.Newtonsoft/JsonBiMapConverter.cs
@@ -0,0 +1,30 @@
+﻿using BidirectionalMap;
+using System;
+using System.Collections.Generic;
+
+namespace Newtonsoft.Json.Converters
+{
+    /// <summary>
+    /// Converts <see cref="BiMap{TDirectKey, TReverseKey}"/> object to or from JSON.
+    /// <para>This converter may be used for BidirectionalMap v1.0.0 package only. In upper package versions <see cref="BiMap{TForwardKey, TReverseKey}"/> implements <see cref="IDictionary{TKey, TValue}"/>.</para>
+    /// </summary>
+    /// <typeparam name="TDirectKey">The type of the direct keys.</typeparam>
+    /// <typeparam name="TReverseKey">The type of the reverse keys.</typeparam>
+    public class JsonBiMapConverter<TDirectKey, TReverseKey> : JsonConverter<BiMap<TDirectKey, TReverseKey>>
+    {
+        public override BiMap<TDirectKey, TReverseKey> ReadJson(JsonReader reader, Type objectType, BiMap<TDirectKey, TReverseKey> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var dictionary = serializer.Deserialize<Dictionary<TDirectKey, TReverseKey>>(reader);
+
+            if (dictionary is null)
+                return null;
+            else
+                return new BiMap<TDirectKey, TReverseKey>(dictionary);
+        }
+
+        public override void WriteJson(JsonWriter writer, BiMap<TDirectKey, TReverseKey> value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value?.Forward?.ToDictionary());
+        }
+    }
+}
---
+++ tests/BidirectionalMap.Json.Newtonsoft.Tests/JsonBiMapConverterTests.cs
@@ -0,0 +1,108 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections;
+
+namespace BidirectionalMap.Serialization.Json.Tests;
+
+[TestClass]
+public class JsonBiMapConverterTests
+{
+    [TestMethod]
+    public void Serialize_NullBiMap_NullJsonValue()
+    {
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new JsonBiMapConverter<char, int>());
+        
+        var map     = (BiMap<char, int>?)null;
+        var mapJson = JsonConvert.SerializeObject(map, settings);
+
+        Assert.AreEqual("null", mapJson);
+    }
+
+    [TestMethod]
+    public void Serialize_FilledBiMap_CorrectJson()
+    {
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new JsonBiMapConverter<char, int>());
+
+        var map = new BiMap<char, int>
+        {
+            { 'a', 1 },
+            { 'b', 2 },
+            { 'c', 3 },
+        };
+
+        var expectedMapJson = @"{""a"":1,""b"":2,""c"":3}";
+        var mapJson         = JsonConvert.SerializeObject(map, settings);
+
+        Assert.AreEqual(expectedMapJson, mapJson);
+    }
+
+    [TestMethod]
+    public void Serialize_FilledBiMapFromDictionary_BiMapJsonEqualsDictionaryJson()
+    {
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new JsonBiMapConverter<char, int>());
+
+        var dictionary = new Dictionary<char, int>
+        {
+            { 'a', 1 },
+            { 'b', 2 },
+            { 'c', 3 },
+        };
+
+        var map            = new BiMap<char, int>(dictionary);
+        var dictionaryJson = JsonConvert.SerializeObject(dictionary, settings);
+        var mapJson        = JsonConvert.SerializeObject(map, settings);
+
+        Assert.AreEqual(dictionaryJson, mapJson);
+    }
+
+    [TestMethod]
+    public void Deserialize_NullJsonValue_NullBiMap()
+    {
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new JsonBiMapConverter<char, int>());
+
+        var mapJson = "null";
+        var map     = JsonConvert.DeserializeObject<BiMap<char, int>>(mapJson, settings);
+
+        Assert.IsNull(map);
+    }
+
+    [TestMethod]
+    public void Deserialize_FilledBiMapJson_CorrectBiMap()
+    {
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new JsonBiMapConverter<char, int>());
+
+        var expecteMap = new BiMap<char, int>
+        {
+            { 'a', 1 },
+            { 'b', 2 },
+            { 'c', 3 },
+        };
+
+        var mapJson = @"{""a"":1,""b"":2,""c"":3}";
+        var map     = JsonConvert.DeserializeObject<BiMap<char, int>>(mapJson, settings)!;
+
+        CollectionAssert.AreEqual((ICollection)expecteMap.Forward.Keys, (ICollection)map.Forward.Keys);
+        CollectionAssert.AreEqual((ICollection)expecteMap.Forward.Values, (ICollection)map.Forward.Values);
+        CollectionAssert.AreEqual((ICollection)expecteMap.Reverse.Keys, (ICollection)map.Reverse.Keys);
+        CollectionAssert.AreEqual((ICollection)expecteMap.Reverse.Values, (ICollection)map.Reverse.Values);
+    }
+
+    [TestMethod]
+    public void Deserialize_FilledDictionaryJson_BiMapEqualsDictionary()
+    {
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new JsonBiMapConverter<char, int>());
+
+        var dictionaryJson = @"{""a"":1,""b"":2,""c"":3}";
+        var dictionary     = JsonConvert.DeserializeObject<Dictionary<char, int>>(dictionaryJson, settings)!;
+        var map            = JsonConvert.DeserializeObject<BiMap<char, int>>(dictionaryJson, settings)!;
+
+        CollectionAssert.AreEqual(dictionary.Keys, (ICollection)map.Forward.Keys);
+        CollectionAssert.AreEqual(dictionary.Values, (ICollection)map.Forward.Values);
+    }
+}
